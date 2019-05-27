7abb13435b1e2cd99c7ed23cee3c5a2813ad6b89
gopavasanth <gopavasanth1999@gmail.com>
2019-05-27 14:23:05 +0530

    Back-End

---
+++ routes/index.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+const Fs = require('fs')
+const Path = require('path')
+const Listr = require('listr')
+const Axios = require('axios')
+const shell = require('shelljs');
+var hash_name = 'video'
+
+if( typeof trim.counter == 'undefined' && typeof tasks.counter == 'undefined' ){
+    trim.counter = 0;
+    tasks.counter = 0;
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ res.render('index', {
+  title: 'VideoCutTool'
+ });
+});
+
+/* Trims the Video using ffmpeg */
+function trim(req, res) {
+ var from_time = req.body.from_time
+ var to_time = req.body.to_time
+
+ var hash_name = 'video'+trim.counter++;
+ var in_location = Path.resolve(__dirname, 'videos', hash_name + '.mp4')
+ var out_location = Path.resolve(__dirname, 'cropped', hash_name + '_trimmed.mp4')
+
+ shell.echo(" " + from_time + " " + to_time + " " + in_location + " " + out_location);
+
+ var cmd = 'ffmpeg -i ' + in_location + ' -ss ' + from_time + ' -t ' + to_time + ' -async 1 ' + out_location;
+ console.log("Command" + cmd);
+
+ if (shell.exec(cmd, (error, stdout, stderr) => {
+   console.log(stdout);
+   console.info("Program Started");
+   console.log(stderr);
+   if (error !== null) {
+    console.log(`Trimming Successuful !`);
+   }
+  }).code !== 0) {
+  shell.echo("Error");
+ }
+ res.render('index', {
+  message: "success"
+ });
+}
+
+/* Downloads the video. */
+async function tasks(req) {
+ const url = req.body.inputVideoUrl
+ var hash_name = 'video'+tasks.counter++;
+ const path = Path.resolve(__dirname, 'videos', hash_name + '.mp4')
+ const writer = Fs.createWriteStream(path)
+
+ const response = await Axios({
+  url,
+  method: 'GET',
+  responseType: 'stream'
+ })
+
+ response.data.pipe(writer)
+ console.log("downloaded")
+ return new Promise((resolve, reject) => {
+  writer.on('finish', resolve)
+  writer.on('error', reject)
+ })
+}
+
+/* Main function to start tasks(downloading the video and trimming the video)*/
+async function start(req) {
+ await tasks(req);
+ trim(req);
+}
+
+router.post('/send', function(req, res, next) {
+ console.log('Hit Send')
+
+ start(req);
+
+});
+
+router.get('/insert', function(req, res, next) {
+ res.sendFile(path.join(__dirname + "/" + "htmlfiles/insert.html"));
+});
+
+module.exports = router;
