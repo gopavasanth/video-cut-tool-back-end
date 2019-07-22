4c348c342b38771f35eec4679e1a85e8efffa7d1
gopavasanth <gopavasanth1999@gmail.com>
2019-07-22 20:44:19 +0300

    Added Sinngle Video to display in front-end

--- routes/index.js
+++ routes/index.js
@@ -72,19 +72,22 @@ function downloadVideo(url, callback) {
   })
 }
 
-function trimVideos( disableAudio, mode, trims, videoPath, callback ) {
+function trimVideos( SinglevideoName, disableAudio, mode, trims, videoPath, callback ) {
 	console.log("==Mode== " + mode)
 	console.log("===disableAudio===" + disableAudio)
 	let videoExtension = videoPath.split('.').pop().toLowerCase();
-	const trimsLocations = [];
 	const trimFuncArray = [];
-
+	const trimsLocations = [];
+	const trimmedVideos = [];
 	trims.forEach((element, index) => {
 
 			trimFuncArray.push(function one(callback) {
 					var hash_name = 'video' + index + Date.now() + '.webm';
-					var out_location = Path.join(__dirname, '/trimmed/', `Trimmed_video_${Date.now()}_${parseInt(Math.random() * 10000)}` + '.' + videoExtension);
+					var videoName = `Trimmed_video_${Date.now()}_${parseInt(Math.random() * 10000)}`
+					var out_location = Path.join(__dirname, '/trimmed/', videoName + '.' + videoExtension);
+					var trimmedvideoName = '/trimmed/' + videoName + '.' + videoExtension;
 					trimsLocations.push(out_location);
+					trimmedVideos.push(trimmedvideoName);
 					Fs.appendFile('myfile', "file '" + out_location + "'\n", (err) => {
 							if (err) throw err;
 							if (disableAudio) {
@@ -98,6 +101,7 @@ function trimVideos( disableAudio, mode, trims, videoPath, callback ) {
 											console.log(error)
 											console.log(`Trimminng Process Completed !`);
 									}
+									console.log("Trimmed Video Names: " + trimmedVideos);
 									callback(null, trimsLocations)
 							}).code !== 0) {
 									shell.echo("Completed");
@@ -109,7 +113,7 @@ function trimVideos( disableAudio, mode, trims, videoPath, callback ) {
 	async.series(trimFuncArray, () => {
 			if (mode === "single") {
 					console.log("I got into Concataion");
-					var out_location = Path.join(__dirname, '/trimmed/', `Concated_video_${Date.now()}_${parseInt(Math.random() * 10000)}` + '.' + videoExtension);
+					var out_location = Path.join(__dirname, '/trimmed/', SinglevideoName + '.' + videoExtension);
 					var cmd = 'ffmpeg -f concat -safe 0 -i myfile -c copy ' + out_location;
 					if (exec(cmd, (error, stdout, stderr) => {
 							if (error !== null) {
@@ -122,6 +126,7 @@ function trimVideos( disableAudio, mode, trims, videoPath, callback ) {
 					callback(null, trimsLocations)
 			}
 	})
+
 }
 
 function rotateVideos(RotatedvideoName, disableAudio, RotateValue, videoPath, callback){
@@ -171,6 +176,70 @@ function cropVideos( CroppedVideoName, disableAudio, req, res, videoPath, callba
 	return callback(null, cropsLocations);
 }
 
+function uploadFileToMediawiki(key, secret, file, options, callback) {
+	if (!callback) {
+		callback = () => { }
+	}
+
+	const token = {
+		key,
+		secret,
+	}
+	return new Promise((resolve, reject) => {
+		// fetch an update csrf token
+
+		const requestData = {
+			url: `${BASE_URL}?action=query&meta=tokens&type=csrf&format=json`,
+			method: 'POST',
+		}
+		request({
+			url: requestData.url,
+			method: requestData.method,
+			headers: oauth.toHeader(oauth.authorize(requestData, token)),
+		}, (err, response, body) => {
+			if (err) {
+				reject(err)
+				return callback(err)
+			}
+			const parsedBody = JSON.parse(body)
+			const csrfToken = parsedBody.query.tokens.csrftoken
+
+			const requestData = {
+				url: `${BASE_URL}?action=upload&ignorewarnings=true&format=json`,
+				method: 'POST',
+				formData: {
+					file,
+					token: csrfToken,
+					...options,
+				},
+			}
+			// perform upload
+			request({
+				url: requestData.url,
+				method: requestData.method,
+				formData: requestData.formData,
+				headers: oauth.toHeader(oauth.authorize(requestData, token)),
+			}, (err, response, body) => {
+				const parsedBody = JSON.parse(body)
+
+				if (parsedBody.error) {
+					reject(parsedBody.error)
+					return callback(parsedBody.error)
+				}
+
+				if (parsedBody.upload && parsedBody.upload.result.toLowerCase() === 'success') {
+					resolve(parsedBody.upload)
+					return callback(null, parsedBody.upload)
+				} else {
+					reject(parsedBody.upload)
+					return callback(parsedBody.upload)
+				}
+			})
+		})
+	})
+};
+
+
 router.post('/video-cut-tool-back-end/send', function(req, res, next) {
   console.log('Hit Send')
 	let RotateValue = req.body.value;
@@ -202,12 +271,25 @@ router.post('/video-cut-tool-back-end/send', function(req, res, next) {
 	   }
 
 			if (videoSettings == "trim") {
-				trimVideos( disableAudio, mode, trims, videoPath, (err, trimmedVideos) => {
-					var response = JSON.stringify({ 
-            message: "Trimming Sucess", 
-						status: "Completed",
-          });
-          res.send(response);
+				var SinglevideoName = `Concated_video_${Date.now()}_${parseInt(Math.random() * 10000)}`;
+				trimVideos( SinglevideoName, disableAudio, mode, trims, videoPath, (err, trimmedVideos) => {
+					if (mode === "multiple"){
+						var response = JSON.stringify({ 
+							message: "Trimming Sucess", 
+							status: "Completed",
+							videoName: trimmedVideos
+						});
+						console.log("Trim Locations: " + trimsLocations);
+						res.send(response);
+					}
+					if (mode === "single"){
+						var response = JSON.stringify({ 
+							message: "Trimming Sucess", 
+							status: "Completed",
+							videoName: 'trimmed/' + SinglevideoName + '.' + videoExtension,
+						});
+						res.send(response);
+					}
         })
 			}
 
