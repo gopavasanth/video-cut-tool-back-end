d664428f44af6860845fb24ef938211bfbe6add4
Ben Houghton <bennixerus@gmail.com>
2020-01-11 21:53:23 +0000

    Add monitoring script

---
+++ monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    start)
+        echo "Starting monitoring.."
+        jstart `dirname "$0"`/bin/monitor
+        echo "Monitoring script started."
+        ;;
+    stop)
+        echo "Stopping monitoring script.."
+        jstop monitor
+        ;;
+    restart)
+        echo "Restarting monitoring script.."
+        jstop monitor
+        jstart `dirname "$0" /bin/monitor`
+        ;;
+    *)
+        echo "./monitor.sh <start/stop/restart>"
+        ;;
